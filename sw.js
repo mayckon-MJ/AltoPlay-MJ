f713d4775b9aef88c4244016d34757dd2de71e4e
Mayckon-Perfil <mayckonj2357@gmail.com>
2026-07-05 15:33:50 -0300

    Add image URL to assets in service worker

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'autoplaymj-v1';
 const ASSETS = [
   './',
   './index.html',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+  'https://i.ibb.co/WJB55vX/119623.png'
 ];
 
 // Instala o Service Worker e guarda o esqueleto inicial no cache
